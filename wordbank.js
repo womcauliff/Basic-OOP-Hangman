cf7bbeb88f48c65927bc1ca16dc643c792de98b1
Will McAuliff <womcauliff@gmail.com>
2016-09-24 11:47:41 -0400

    Adjusting module exports object

--- wordbank.js
+++ wordbank.js
@@ -6,34 +6,11 @@
  * This contains all of the video game titles for Hangman game rounds.
  */
 
-exports.wordlist = [
+module.exports = [
 	"Goldeneye",
-    "NBA Jam",
     "Doom",
     "Quake",
     "Unreal",
     "Diablo",
-    "NFL Blitz",
-    "Tekken",
-    "Crash Bandicoot",
-    "Street Fighter II",
-    "Mega Man X",
-    "Super Metroid",
-    "Donkey Kong Country",
-    "Super Mario Cart",
-    "Mortal Kombat",
-    "Earthworm Jim",
-    "Chrono Trigger",
-    "Battletoads",
-    "Castlevania",
-    "Final Fantasy VII",
-    "Super Mario World",
-    "Legend of Zelda",
-    "Age of Empires",
-    "Metal Gear Solid",
-    "Sonic the Hedgehog",
-    "Secret of Mana",
-    "Zombies Ate My Neighbors",
-    "Super Mario RPG",
-    "Killer Instinct"
+    "Tekken"
 ];
