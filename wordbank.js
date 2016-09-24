57dc9624ce64f0d05eef66f8bf60740c64c83166
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-09-24 10:03:40 -0400

    Update wordlist array.

--- wordbank.js
+++ wordbank.js
@@ -1,7 +1,39 @@
-var wordlist = [
-	"cat",
-	"bat",
-	"dog",
-	"rat"
+/**
+ * Hangman Wordbank
+ * UCF Coding Bootcamp 2016
+ * By: Ronny Tomasetti
+ *
+ * This contains all of the video game titles for Hangman game rounds.
+ */
+
+exports.wordlist = [
+	"Goldeneye",
+    "NBA Jam",
+    "Doom",
+    "Quake",
+    "Unreal",
+    "Diablo",
+    "NFL Blitz",
+    "Tekken",
+    "Crash Bandicoot",
+    "Street Fighter II",
+    "Mega Man X",
+    "Super Metroid",
+    "Donkey Kong Country",
+    "Super Mario Cart",
+    "Mortal Kombat",
+    "Earthworm Jim",
+    "Chrono Trigger",
+    "Battletoads",
+    "Castlevania",
+    "Final Fantasy VII",
+    "Super Mario World",
+    "Legend of Zelda",
+    "Age of Empires",
+    "Metal Gear Solid",
+    "Sonic the Hedgehog",
+    "Secret of Mana",
+    "Zombies Ate My Neighbors",
+    "Super Mario RPG",
+    "Killer Instinct"
 ];
-module.exports = wordlist;
